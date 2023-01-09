b593ceefdbb8f69d3e90f16d86a8746fe2a268d6
Alexandr Zubakin <zubakin.alexandr98@gmail.com>
2023-01-09 18:00:06 +0700

    edit index view

--- menu/views.py
+++ menu/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 
 def index(request):
-    return render(request, "index.html")
+    menus = Menu.objects.all()
+    return render(request, "index.html", {'items': menus})
+
+def menu_view(request, pk):
+    pass
