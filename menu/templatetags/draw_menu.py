d05fbd98450094b94fd7e200e2769df023dab4e6
Alexandr Zubakin <zubakin.alexandr98@gmail.com>
2023-01-09 19:56:30 +0700

    edit tags

--- menu/templatetags/draw_menu.py
+++ menu/templatetags/draw_menu.py
@@ -10,10 +10,19 @@ register = template.Library()
 def show_menu(pk):
     menu = models.Menu.objects.get(pk=pk)
     try:
-        resp = {}
-        items = models.MenuItem.objects.filter(menu=menu)
-        print(items)
-        resp['menu'] = items
+        resp = {"menu": []}
+        items = models.MenuItem.objects.filter(menu=menu).filter(parent=None)
+        for item in items:
+            children = models.MenuItem.objects.filter(parent=item)
+            flag = False
+            if children:
+                flag = True
+            resp["menu"].append({
+                "name": item.name,
+                'children': children,
+                'has_children': flag
+
+            })
         return resp
     except models.Menu.DoesNotExist:
         raise template.TemplateSyntaxError("This menu is not found")
