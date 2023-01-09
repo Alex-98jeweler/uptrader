e254f4b8d52041d0a9c039e52c7572b721028ba8
Alexandr Zubakin <zubakin.alexandr98@gmail.com>
2023-01-09 17:37:54 +0700

    edit db properties

--- menu/models.py
+++ menu/models.py
@@ -4,9 +4,15 @@ from django.db import models
 class Menu(models.Model):
     name = models.CharField(max_length=256, unique=True, db_index=True)
 
+    def __str__(self) -> str:
+        return self.name
+
 
 
 class MenuItem(models.Model):
     name = models.CharField(max_length=256)
     menu = models.ForeignKey(Menu, on_delete=models.CASCADE)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
 
+    def __str__(self) -> str:
+        return self.name
